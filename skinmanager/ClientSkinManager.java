5646b14ddd681f3cb6d82c03eab5975cd9f2e62c
Poyraz <67389553+zPoyraz@users.noreply.github.com>
2021-01-07 15:26:00 +0300

    Update ClientSkinManager.java

--- skinmanager/ClientSkinManager.java
+++ skinmanager/ClientSkinManager.java
@@ -1,7 +1,45 @@
 package yourclient.skinmanager;
 
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
+import com.mojang.authlib.minecraft.MinecraftProfileTexture.Type;
+
+import net.minecraft.client.resources.SkinManager.SkinAvailableCallback;
+
 public class ClientSkinManager {
 
-
-
+	public static ClientSkinManager instance = new ClientSkinManager();
+	
+	private static JsonObject skinJson;
+	private static JsonParser jsonParser = new JsonParser();
+	
+	public void getSkin(GameProfile gameprofile, Map skinMap, SkinAvailableCallback callback) {
+		if(skinJson == null) {
+			try {
+				HttpsURLConnection connection = (HttpsURLConnection) new URL("Verilerin Alınacağı Site").openConnection();
+				connection.connect();
+				skinJson = jsonParser.parse(new InputStreamReader(connection.getInputStream())).getAsJsonObject();
+			}catch(Exception e) {
+				System.out.println("Veriler alınamadı.");
+				e.printStackTrace();
+				return;
+			}
+			try {
+				if(skinJson.has(gameprofile.getName())) {
+					skinMap.put(Type.CAPE, new MinecraftProfileTexture(skinJson.get(gameprofile.getName()).getAsString(), null));
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
